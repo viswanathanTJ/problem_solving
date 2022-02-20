e46f37522977a8da0a947fb5eef4d95353569090
viswanathanTJ <viswanathantj@gmail.com>
2022-02-20 18:59:14 +0530

    LinkedList C

--- Python/solution.py
+++ Python/solution.py
@@ -1,16 +1,8 @@
-class Node:
-   def __init__(self, dataval=None):
-      self.dataval = dataval
-      self.nextval = None
-
-class SLinkedList:
-   def __init__(self):
-      self.headval = None
-
-list1 = SLinkedList()
-list1.headval = Node("Mon")
-e2 = Node("Tue")
-e3 = Node("Wed")
-list1.headval.nextval = e2
-
-e2.nextval = e3
+l = []
+l.append(5)
+l.append(4)
+l.append(3)
+l.append(2)
+l.append(1)
+l.pop()
+print(l)
