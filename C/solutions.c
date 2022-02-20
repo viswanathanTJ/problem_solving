4586a6fbe31c83318f2e5c51dffc02bfe2884c32
viswanathanTJ <viswanathantj@gmail.com>
2022-02-20 17:25:10 +0530

    Aligned

--- C/solutions.c
+++ C/solutions.c
@@ -1,7 +1,49 @@
-#include<stdio.h>
-int main()
+#include <stdio.h>
+#include <malloc.h>
+#include <stdlib.h>
+ 
+void main()
 {
-int o=5,e=0;
-if(o&1) printf("Odd\n");
-if(!(e&1)) printf("Even");
+    typedef struct Node
+    {
+        int num;
+        struct node *ptr;
+    }node;
+ 
+    node *head, *first, *temp = 0;
+    int count = 0;
+    int choice = 1;
+    first = 0;
+ 
+    while (choice)
+    {
+        head  = (node *)malloc(sizeof(node));
+        printf("Enter the data item\n");
+        scanf("%d", &head-> num);
+        if (first != 0)
+        {
+            temp->ptr = head;
+            temp = head;
+        }
+        else
+        {
+            first = temp = head;
+        }
+        fflush(stdin);
+        printf("Do you want to continue(Type 0 or 1)?\n");
+        scanf("%d", &choice);
+ 
+    }
+    temp->ptr = 0;
+    /*  reset temp to the beginning */
+    temp = first;
+    printf("\n status of the linked list is\n");
+    while (temp != 0)
+    {
+        printf("%d=>", temp->num);
+        count++;
+        temp = temp -> ptr;
+    }
+    printf("NULL\n");
+    printf("No. of nodes in the list = %d\n", count);
 }
--- Python/solution.py
+++ Python/solution.py
@@ -1,32 +1,16 @@
-from numpy import sort
+class Node:
+   def __init__(self, dataval=None):
+      self.dataval = dataval
+      self.nextval = None
 
+class SLinkedList:
+   def __init__(self):
+      self.headval = None
 
-def h_index(n, citations):
-  ans = []
-  # ans = []
-  r = 0
-  citations.sort()
-  # 1 2 2 3 3 15
-  for i in range(n):
-    # sorted_c = sorted(citations[2:n])
-    # if sorted_c[-1] >= i:
-    print(citations[i], i)
-    if citations[i] > i:
-        r = r + 1
-        ans.append(r)
-    else:
-        ans.append(r)
-  return ans
+list1 = SLinkedList()
+list1.headval = Node("Mon")
+e2 = Node("Tue")
+e3 = Node("Wed")
+list1.headval.nextval = e2
 
-if __name__ == '__main__':
-  t = int(input())                           
-  # t = 1                                           
-  for test_case in range(1, t + 1):
-    n = int(input())
-    # n = 6
-    # citations = [1, 2, 2, 3, 3, 15]
-    citations = list(map(int, input().split()))
-    print(citations)
-    h_index_scores = h_index(n, citations)
-    print("Case #" + str(test_case) + ": " +
-          ' '.join(map(str, h_index_scores)))
+e2.nextval = e3
