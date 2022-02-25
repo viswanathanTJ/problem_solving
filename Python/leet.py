46b48855a34a7e6204fdf969c7bb5eeec25ec6cb
viswanathantj <viswanathantj@gmail.com>
2022-02-25 14:18:56 +0530

    Alternate leet

--- Python/leet.py
+++ Python/leet.py
@@ -15,4 +15,17 @@ def solve(version1, version2):
     return 0
 
 
-print(solve("1.0.1", "1"))
+def soln(version1, version2):
+    v1, v2 = version1.split('.'), version2.split('.')
+    m, n = len(v1), len(v2)
+    i = j = 0
+    while i < m or j < n:
+        r1 = int(v1[i]) if i < m else 0
+        r2 = int(v2[i]) if j < n else 0
+        if r1 < r2: return -1
+        elif (r1 > r2): return 1
+        i += 1
+        j += 1
+    return 0
+
+print(soln("1.0.1", "1"))
