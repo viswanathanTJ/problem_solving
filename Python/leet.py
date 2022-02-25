7cfffdac44fc1fdd90f51fb8101145dc531c04a8
viswanathantj <viswanathantj@gmail.com>
2022-02-25 15:01:59 +0530

    Integer Roman

--- Python/leet.py
+++ Python/leet.py
@@ -1,31 +1,23 @@
-def solve(version1, version2):
-    a = list(map(int, version1.split('.')))
-    b = list(map(int, version2.split('.')))
-    while len(a) > len(b):
-        b.append(0)
-    while len(b) > len(a):
-        a.append(0)
-    for i in range(len(a)):
-        if a[i] == b[i]:
-            continue
-        elif a[i] < b[i]:
-            return -1
-        elif a[i] > b[i]:
-            return 1
-    return 0
+def intToRoman(num):
+    d = {1000: 'M', 900: 'CM', 500: 'D', 400: 'CD', 100:'C', 90:'XC', 50: 'L', 40:'XL', 10: 'X', 9:'IX', 5:'V', 4:'IV', 1: 'I'}
+    for key in d.keys():
+        if num >= key:
+            return str(d[key]) + str(intToRoman(num - key))
+    return ''
+
+def romanToInt(s):
+    d = {'M':1000,'D':500,'C':100,'L':50,'X':10,'V':5,'I':1}
+    res = 0
+    for x in reversed(s):
+        if x =='C':
+            res += d[x] * (-1 if res >= 500 else 1)
+        elif x == 'X':
+            res += d[x] * (-1 if res >= 50 else 1)
+        elif x == 'I':
+            res += (-1 if res >= 5 else 1)
+        else:
+            res += d[x]
+    return res
 
 
-def soln(version1, version2):
-    v1, v2 = version1.split('.'), version2.split('.')
-    m, n = len(v1), len(v2)
-    i = j = 0
-    while i < m or j < n:
-        r1 = int(v1[i]) if i < m else 0
-        r2 = int(v2[i]) if j < n else 0
-        if r1 < r2: return -1
-        elif (r1 > r2): return 1
-        i += 1
-        j += 1
-    return 0
-
-print(soln("1.0.1", "1"))
+print(romanToInt('MCMXCIV'))
