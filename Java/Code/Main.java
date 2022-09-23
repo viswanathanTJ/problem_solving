20e5570f4bb0f0291b5782515f5d00a84e909a99
viswanathanTJ <viswanathantj@gmail.com>
2022-09-24 00:42:32 +0530

    Leetcode

--- Java/Code/Main.java
+++ Java/Code/Main.java
@@ -1,11 +1,32 @@
-interface A{
-    int x=65;
-}
-interface B{
-    int x=66;
-}
-public class Main implements A,B {
-    public static void main(String[] a){
-        System.out.println(B.x); // which x?
+class Main {
+    static void getPermutations(int[] arr) {
+        helper(arr, 0);
+    }
+
+    static void helper(int[] arr, int pos) {
+        if(pos > arr.length-1) {
+            for(int i=0;i<pos;i++) {
+                System.out.print(arr[i]+",");
+            }
+            System.out.println();
+        }
+        for(int i=pos;i<arr.length;i++) {
+            int t = arr[pos];
+            arr[pos] = arr[i];
+            arr[i] = t;
+
+            helper(arr, pos+1);
+
+            t = arr[pos];
+            arr[pos] = arr[i];
+            arr[i] = t;
+        }
+        
+    }
+
+
+
+    public static void main(String[] args) {
+        getPermutations(new int[] {1,2,3,4});
     }
 }
