8e563ce42e89111867d29c2b6d82389a81b20ddb
viswanathantj <viswanthan.tj@zohocorp.com>
2024-06-20 22:08:26 +0530

    two sums

--- Python/solution.py
+++ Python/solution.py
@@ -1,17 +1,30 @@
-for _ in range(int(input())):
-    n, l = int(input()), list(map(int, input().split()))
-    res = 0
-    m = 0
-    if n == 2:
-        print(l[0]+l[1]+1)
-        continue
-    s = sorted(l, reverse=True)
-    for i in range(n):
-        if l[i] > m:
-            for j in range(i+1, n):
-                ans = l[i] + l[j] + j - i
-                res = max(ans, res)
-            m = l[i]
-    print(res)
+from typing import List
+import math
+from icecream import ic
+import sys
+import os
 
-    
+
+class Solution:
+    def two_sums(nums, target):
+        value_ind = {}
+        for i in range(len(nums)):
+            value = target - nums[i]
+            if value in value_ind:
+                return [i, value_ind[value]]
+            value_ind[nums[i]] = i
+
+
+
+## execution codes
+
+solution = Solution()
+
+with open('input.txt', 'r') as f:
+    while True:
+        l1 = f.readline().strip()
+        if not l1: break
+        l = list(map(int, l1.split(',')))
+        t = int(f.readline().strip())
+        res = Solution.two_sums(l, t)
+        print(res)
