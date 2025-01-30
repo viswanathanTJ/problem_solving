c5bee83de511ac04e6a45971b8e05f614b3c18a7
viswanathantj <viswanathantj@gmail.com>
2025-01-30 23:52:03 +0530

    sort colors

--- Java/Code/Solution.java
+++ Java/Code/Solution.java
@@ -1,31 +1,38 @@
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 
 public class Solution {
 
-    public static int kadane(int nums[]) throws Exception {
-        int max = nums[0], sum = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (sum < 0) sum = 0;
-            sum = sum + nums[i];
-            if (sum > max) max = sum;
+    public static void sortColors(int[] nums) {
+        int n = nums.length;
+        int l = 0, m = 0, r = n - 1;
+        while (m <= r) {
+            int t = nums[m];
+            if (t == 0) {
+                nums[m] = nums[l];
+                nums[l] = t;
+                l++; m++;
+            } else if (nums[m] == 2) {
+                nums[m] = nums[r];
+                nums[r] = t;
+                r--;
+            } else {
+                m++;
+            }
         }
-        return max;
     }
 
     public static void main(String args[]) throws Exception {
         Scanner sn = new Scanner(System.in);
         int testCase = sn.nextInt();
         sn.nextLine(); // consume leftover newline
-        // System.out.println(factorial(6));
-        int[] inp = new int[]{-2,1,-3,4,-1,2,1,-5,4};
-        // kadane(inp);
-        int[] inp1 = new int[]{-2, 1};
-        System.out.println(kadane(inp1));
-
-
+        for (int i = 0; i < testCase; i++) {
+            String s = sn.nextLine();
+            String[] arr = s.substring(1, s.length() - 1).split(",");
+            int[] nums = Arrays.stream(arr).mapToInt(Integer::parseInt).toArray();
+            sortColors(nums);
+            System.out.println(Arrays.toString(nums));
+        }
         sn.close();
     }
 }
