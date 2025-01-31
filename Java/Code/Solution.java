7e8450cd0d809ad6858cce50e32e5b43730c4b56
viswanathantj <viswanathantj@gmail.com>
2025-01-31 10:47:37 +0530

    max profit

--- Java/Code/Solution.java
+++ Java/Code/Solution.java
@@ -3,23 +3,16 @@ import java.util.Scanner;
 
 public class Solution {
 
-    public static void sortColors(int[] nums) {
-        int n = nums.length;
-        int l = 0, m = 0, r = n - 1;
-        while (m <= r) {
-            int t = nums[m];
-            if (t == 0) {
-                nums[m] = nums[l];
-                nums[l] = t;
-                l++; m++;
-            } else if (nums[m] == 2) {
-                nums[m] = nums[r];
-                nums[r] = t;
-                r--;
-            } else {
-                m++;
-            }
+    public static int maxProfit(int[] prices) {
+        int n = prices.length;
+        int profit = Integer.MIN_VALUE;
+        int lowest_price = prices[0];
+        for (int i = 0; i < n; i++) {
+            if (prices[i] < lowest_price) lowest_price = prices[i];
+            else  profit = Math.max(prices[i] - lowest_price, profit);
         }
+
+        return profit >= 0 ? profit : 0;
     }
 
     public static void main(String args[]) throws Exception {
@@ -30,8 +23,8 @@ public class Solution {
             String s = sn.nextLine();
             String[] arr = s.substring(1, s.length() - 1).split(",");
             int[] nums = Arrays.stream(arr).mapToInt(Integer::parseInt).toArray();
-            sortColors(nums);
-            System.out.println(Arrays.toString(nums));
+            int res = maxProfit(nums);
+            System.out.println(res);
         }
         sn.close();
     }
