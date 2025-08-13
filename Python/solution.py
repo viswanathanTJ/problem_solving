c5b6d33605d66fdc986db109862ff9d11738c0ea
viswanathanTJ <viswanathantj@gmail.com>
2025-08-13 22:06:43 +0530

    merge intervals

--- Python/solution.py
+++ Python/solution.py
@@ -1,32 +1,48 @@
-s, n = list(input()), int(input())
-res = ''
+class Solution:
+    def merge(self, intervals):
+        n = len(intervals)
+        res = []
+        intervals.sort()
+        
+        for i in range(n):
+            if not res or res[-1][1] < intervals[i][0]:
+                res.append(intervals[i])
+            else:
+                res[-1] = [res[-1][0], max(intervals[i][1], res[-1][-1])]
+        return res
+            
+        
+    def merge1(self, intervals):
+        res = []
+        n = len(intervals)
+        intervals.sort()
 
-print(s, n)
-t = []
+        for i in range(n):
+            start, end = intervals[i]
 
-t = ''
-count = 1
-d = {}
-for c in s:
-    if c == t:
-        if c in d
+            if res and res[-1][1] >= end:
+                continue
+            
+            for j in range(i+1, n):
+                if end >= intervals[j][0]:
+                    end = max(end, intervals[j][1])
+                else:
+                    break
+            
+            res.append([start, end])
+            
+        return res
 
+    
 
-
-
-
-def remove_consec_duplicates(s):
-    new_s = ""
-    prev = ""
-    for c in s:
-        count = 1
-        if len(new_s) == 0:
-            new_s += c
-            prev = c
-        if c == prev:
-            count += 1
-            continue
-        else:
-            new_s += c
-            prev = c
-    return new_s
+import ast
+ 
+l = ast.literal_eval(input())
+res = Solution().merge(l)
+print(res)
+l = ast.literal_eval(input())
+res = Solution().merge(l)
+print(res)
+l = ast.literal_eval(input())
+res = Solution().merge(l)
+print(res)
