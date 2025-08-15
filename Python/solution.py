753f618c638787ccc8f05224d57256c2c07760c4
viswanathanTJ <viswanathantj@gmail.com>
2025-08-15 16:32:54 +0530

    merge sorted arrays

--- Python/solution.py
+++ Python/solution.py
@@ -1,41 +1,74 @@
 from typing import List
 
 class Solution:
-    def findDuplicate(self, nums: List[int]) -> int:
-        slow_pointer = nums[0]
-        fast_pointer = nums[0]
+    def sortArrays(self, nums1, m, nums2, n):
+        p1, p2, ins = m-1, n-1, m + n - 1
+        while p1 > -1 and p2 > -1:
+            if nums1[p1] > nums2[p2]:
+                nums1[ins] = nums1[p1]
+                p1 -= 1
+            else:
+                nums1[ins] = nums2[p2]
+                p2 -= 1
+            ins -= 1
         
-        while True:
-            slow_pointer = nums[slow_pointer]
-            fast_pointer = nums[nums[fast_pointer]]
-            if fast_pointer == slow_pointer:
+        while p2 > -1:
+            nums1[ins] = nums2[p2]
+            p2 -= 1
+            ins -= 1
+        
+                
+        
+    def sortArraysNoSpace(self, nums1, m, nums2, n):
+        # no extra space
+        left, right = m - 1, 0
+        while left > -1 and right < n:
+            if nums1[left] > nums2[right]:
+                nums1[left], nums2[right] = nums2[right], nums1[left]
+                left -= 1
+                right += 1
+            else:
                 break
 
-        fast_pointer = nums[0]
-        
-        while fast_pointer != slow_pointer:
-            slow_pointer = nums[slow_pointer]
-            fast_pointer = nums[fast_pointer]
+        for i in range(m):
+            for j in range(m):
+                if nums1[i] < nums1[j]:
+                    nums1[i], nums1[j] = nums1[j], nums1[i]
 
-        return fast_pointer
+        nums2.sort()
+        ind = 0
+        for i in range(m, m+n):
+            nums1[i] = nums2[ind]
+            ind += 1
         
-    def findDuplicateNoob(self, nums: List[int]) -> int:
-        s = set()
-        for n in nums:
-            if n in s:
-                return n
-            s.add(n)
-        return -1
-    
+        
+    def sortArraysNaive(self, nums1, m, nums2, n):
+        res = [0] * (m+n)
+        i, i1, i2 = 0, 0, 0
+        while i1 < m and i2 < n:
+            if nums1[i1] <= nums2[i2]:
+                res[i] = nums1[i1]
+                i1 += 1
+            else:
+                res[i] = nums2[i2]
+                i2 += 1
+            i += 1
+        
+        while i1 < m:
+            res[i] = nums1[i1]
+            i1 += 1
+            i += 1
+        while i2 < n:
+            res[i] = nums2[i2]
+            i2 += 1
+            i += 1
+        
+        for i in range(m+n):
+            nums1[i] = res[i]
+            
 
 import ast
  
-l = ast.literal_eval(input())
-res = Solution().findDuplicate(l)
-print(res)
-l = ast.literal_eval(input())
-res = Solution().findDuplicate(l)
-print(res)
-l = ast.literal_eval(input())
-res = Solution().findDuplicate(l)
-print(res)
+nums1 = ast.literal_eval(input())
+res = Solution().sortArrays(nums1, int(input()), ast.literal_eval(input()), int(input()))
+print(nums1)
