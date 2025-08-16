deea02bfaa420f19f2fc83929e738e8550b25c0d
viswanathanTJ <viswanathantj@gmail.com>
2025-08-16 18:50:27 +0530

    find num in matrix

--- Python/solution.py
+++ Python/solution.py
@@ -1,74 +1,34 @@
 from typing import List
 
 class Solution:
-    def sortArrays(self, nums1, m, nums2, n):
-        p1, p2, ins = m-1, n-1, m + n - 1
-        while p1 > -1 and p2 > -1:
-            if nums1[p1] > nums2[p2]:
-                nums1[ins] = nums1[p1]
-                p1 -= 1
+    def searchMatrix(self, matrix, target):
+        n = len(matrix)
+        m = len(matrix[0])
+
+        left = 0
+        right = (n * m) - 1
+        while left <= right:
+            mid = left + (right - left) // 2
+            row_ind = mid // m
+            col_ind = mid % m
+
+            num = matrix[row_ind][col_ind]
+
+            if num == target: return True
+
+            if num > target:
+                right = mid - 1
             else:
-                nums1[ins] = nums2[p2]
-                p2 -= 1
-            ins -= 1
-        
-        while p2 > -1:
-            nums1[ins] = nums2[p2]
-            p2 -= 1
-            ins -= 1
-        
+                left = mid + 1
                 
-        
-    def sortArraysNoSpace(self, nums1, m, nums2, n):
-        # no extra space
-        left, right = m - 1, 0
-        while left > -1 and right < n:
-            if nums1[left] > nums2[right]:
-                nums1[left], nums2[right] = nums2[right], nums1[left]
-                left -= 1
-                right += 1
-            else:
-                break
+        return False
 
-        for i in range(m):
-            for j in range(m):
-                if nums1[i] < nums1[j]:
-                    nums1[i], nums1[j] = nums1[j], nums1[i]
-
-        nums2.sort()
-        ind = 0
-        for i in range(m, m+n):
-            nums1[i] = nums2[ind]
-            ind += 1
-        
-        
-    def sortArraysNaive(self, nums1, m, nums2, n):
-        res = [0] * (m+n)
-        i, i1, i2 = 0, 0, 0
-        while i1 < m and i2 < n:
-            if nums1[i1] <= nums2[i2]:
-                res[i] = nums1[i1]
-                i1 += 1
-            else:
-                res[i] = nums2[i2]
-                i2 += 1
-            i += 1
-        
-        while i1 < m:
-            res[i] = nums1[i1]
-            i1 += 1
-            i += 1
-        while i2 < n:
-            res[i] = nums2[i2]
-            i2 += 1
-            i += 1
-        
-        for i in range(m+n):
-            nums1[i] = res[i]
-            
 
 import ast
  
-nums1 = ast.literal_eval(input())
-res = Solution().sortArrays(nums1, int(input()), ast.literal_eval(input()), int(input()))
-print(nums1)
+for _ in range(4):
+    nums1 = ast.literal_eval(input())
+    target = int(input())
+    print(nums1, target)
+    res = Solution().searchMatrix(nums1, target)
+    print(res)
