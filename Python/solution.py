0e2113d769bd067529553e406c81c3f82654006e
viswanathanTJ <viswanathantj@gmail.com>
2025-08-26 21:09:18 +0530

    quick sort

--- Python/solution.py
+++ Python/solution.py
@@ -71,7 +71,40 @@ def merge_sort(arr, low, high):
     # solving
     merge(arr, low, mid, high)
 
+'''
+2 3 5 11 23 4 5 12
+
+12
+2
+3
+4
+11
+23 => 12 and 23 swapp
+12
+4 => 12 and 4 swap
+5 => 12 and 5 swap
+'''
+def get_partition(arr, low, high):
+    i = low - 1
+    pivot = arr[high]
+
+    for k in range(low, high):
+        if arr[k] < pivot:
+            i += 1
+            arr[k], arr[i] = arr[i], arr[k]
+
+    arr[i+1], arr[high] = arr[high], arr[i+1]
+    return i + 1
+
+
+def quick_sort(arr, low, high):
+    if low < high:
+        pIndex = get_partition(arr, low, high)
+        quick_sort(arr, low, pIndex - 1)
+        quick_sort(arr, pIndex + 1, high)
+
+
 a = [3,4,2,1,9,5,6,22,20]
 print(len(a))
-merge_sort(a, 0, len(a) - 1)
+quick_sort(a, 0, len(a) - 1)
 print(a)
