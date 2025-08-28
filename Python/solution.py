d37f73389a5c369a1506ad3ce2027bd6cc4b7534
viswanathanTJ <viswanathantj@gmail.com>
2025-08-28 22:03:49 +0530

    rearrange pos neg

--- Python/solution.py
+++ Python/solution.py
@@ -2,13 +2,19 @@ from typing import List
 import ast
 
 class Solution:
-    def maxSubArray(self, nums: List[int]) -> int:
-        res, s = float('-inf'), 0
-        for n in nums:
-            s += n
-            res = max(s, res)
-            if s < 0: s = 0
+    def rearrangeArray(self, nums: List[int]) -> int:
+        res = [0] * len(nums)
+        pind, nind = 0, 1
+        for i in range(len(nums)):
+            if nums[i] < 0:
+                res[nind] = nums[i]
+                nind += 2
+            else:
+                res[pind] = nums[i]
+                pind += 2
+        
         return res
 
-res = Solution().maxSubArray(ast.literal_eval(input()))
+
+res = Solution().rearrangeArray(ast.literal_eval(input()))
 print(res)
