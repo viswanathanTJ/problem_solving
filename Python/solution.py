15823ed1ea07ed1d2646b67ed2502db356f46e6b
viswanathanTJ <viswanathantj@gmail.com>
2025-10-31 12:54:35 +0530

    max area

--- Python/solution.py
+++ Python/solution.py
@@ -3,37 +3,28 @@ import ast
 from collections import defaultdict, Counter
 
 class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
-        n = len(nums)
-        nums.sort()
-        res = []
 
-        for i in range(n - 2):
-            if i > 0 and nums[i] == nums[i - 1]:
-                continue
+    def maxArea(self, height: List[int]) -> int:
+        l, h = 0, len(height) - 1
+        res = -1
 
-            l = i + 1
-            h = n - 1
-            num = nums[i]
-            while l < h:
-                s = num + nums[l] + nums[h]
-                if s == 0:
-                    triplet = [num, nums[l], nums[h]]
-                    res.append(triplet)
-                    while l < h and nums[l] == nums[l+1]:
-                        l += 1
-                    while l < h and nums[h] == nums[h-1]:
-                        h -= 1
-                    l += 1
-                    h -= 1
-                elif s > 0:
-                    h -= 1
-                else:
-                    l += 1
-                
+        while l < h:
+            low_height = min(height[h], height[l])
+            length = low_height * abs(h - l)
+            res = max(res, length)
+            if height[h] >= height[l]:
+                l += 1
+            else:
+                h -= 1
+
         return res
 
-
+            
+            
+            
+            
+            
+            
 
 
         pass
@@ -41,5 +32,5 @@ class Solution:
         
 # for _ in range(3):
 inp = ast.literal_eval(input())
-res = Solution().threeSum(inp)
+res = Solution().maxArea(inp)
 print(res)
