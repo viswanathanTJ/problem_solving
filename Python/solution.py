e14458e3e37bebd84981f0ff3b0bbd5885dbbfc1
viswanathanTJ <viswanathantj@gmail.com>
2025-11-03 12:18:13 +0530

    rearrange array

--- Python/solution.py
+++ Python/solution.py
@@ -6,41 +6,31 @@ import math
 
 class Solution:
 
-    def majorityElement(self, nums):
-        d = defaultdict(int)
-
-        for n in nums:
-            d[n] = d[n] + 1
-
-        sorted_d = sorted(d.items(), key = lambda x : x[1], reverse=True)
+    def rearrangeArray(self, nums):
+        # TC: O(n)
+        # SC: O(n)
+        pind, nind = 0, 1
+        n = len(nums)
+        res = [0] * n
         
-        return sorted_d.pop(0)[0]
-        
-
-        pass
-
-    def majorityElementOptimal(self, nums):
-        cnt = 0
-        el = None
-
-        for n in nums:
-            if not el or not cnt:
-                el = n
-                cnt = 1
-            elif el == n:
-                cnt += 1
+        for i in range(n):
+            if nums[i] < 0:
+                if nind < n:
+                    res[nind] = nums[i]
+                    nind += 2
             else:
-                cnt -= 1
-
-        return el
-
-
-
+                if pind < n:
+                    res[pind] = nums[i]
+                    pind += 2
+        
+        return res
+        
+        
         pass
          
 # for _ in range(3):
 inp = ast.literal_eval(input())
-res = Solution().majorityElement(inp)
+res = Solution().rearrangeArray(inp)
 # inp1 = int(input())
 # res = Solution().majorityElement(inp, inp1)
 print(res)
