961cc79b9d63ecaf0c1efff73e858213bee11e03
viswanathanTJ <viswanathantj@gmail.com>
2025-11-08 14:09:49 +0530

    rotate matrix

--- Python/solution.py
+++ Python/solution.py
@@ -5,63 +5,38 @@ from icecream import ic
 import math
 
 class Solution:
+    def printMatrix(self, matrix):
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                print(matrix[i][j], end=" ")
+            print()
 
-    def pascalTriangleI(self, r, c):
-        n = r
-        r = c
+    def rotate(self, matrix: List[List[int]]) -> None:
+        n = len(matrix)
+        for i in range(n):
+            for j in range(i):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 
-        if r < n - r:
-            r = n - r
+        self.printMatrix(matrix)
+            
+        for i in range(n):
+            for j in range(n // 2):
+                matrix[i][j], matrix[i][n-1-j] = matrix[i][n-1-j], matrix[i][j]
 
-        res = 1
-        for i in range(r):
-            res *= (n - i)
-            ic(n - i)
-            res //= (i + 1)
-        
-        return res
+        self.printMatrix(matrix)
 
+    def rotateBrute(self, matrix: List[List[int]]) -> None:
+        self.printMatrix(matrix)
+        mat = [[matrix[len(matrix) - 1 - j][i] for j in range(len(matrix[0]))] for i in range(len(matrix))]
+        matrix = mat
+        self.printMatrix(matrix)
+        return matrix
 
-        pass
-         
-    def pascalTriangleII(self, r):
-        res = [1]
-        for i in range(1, r):
-            res.append((res[-1]*(r - i)) // (i))
-        return res
-
-    def pascalTriangleIIBetter(self, r):
-        n = r - 1
-        def nCr(n, r):
-            res = 1
-            for i in range(r):
-                res *= (n - i)
-                res //= (i + 1)
-            return res
-
-        half = n // 2
-        half += 1
-
-        res = [nCr(n, i) for i in range(half)]
-        res.extend(res[::-1] if n % 2 else res[:-1][::-1])
-        
-        return res
-    
-    def pascalTriangleIII(self, n):
-        res = [[1]]
-        for i in range(1, n + 1):
-            row = [1]
-            for j in range(1, i):
-                row.append(res[-1][j] + res[-1][j-1])
-            row.append(1)
-            res.append(row)
-        return res
 
 # for _ in range(3):
-# inp = ast.literal_eval(input())
-# res = Solution().rearrangeArray(inp)
-inp1 = int(input())
+inp = ast.literal_eval(input())
+res = Solution().rotate(inp)
+# inp1 = int(input())
 # inp2 = int(input())
 # res = Solution().pascalTriangleI(inp1, inp2)
-res = Solution().pascalTriangleIII(inp1)
 print(res)
