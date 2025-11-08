7aa9702a2f0947b434c2c0ef0b286b85d11eba3f
viswanathanTJ <viswanathantj@gmail.com>
2025-11-08 15:20:42 +0530

    3 sum

--- Python/solution.py
+++ Python/solution.py
@@ -5,37 +5,37 @@ from icecream import ic
 import math
 
 class Solution:
-    def printMatrix(self, matrix):
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
-                print(matrix[i][j], end=" ")
-            print()
+    def threeSum(self, nums):
+        nums.sort()
+        res = []
+        print(nums)
 
-    def rotate(self, matrix: List[List[int]]) -> None:
-        n = len(matrix)
-        for i in range(n):
-            for j in range(i):
-                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        for i in range(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            l = i + 1
+            h = len(nums) - 1
+            while l < h:
+                addition = nums[i] + nums[l] + nums[h]
+                if addition > 0:
+                    h -= 1
+                elif addition < 0:
+                    l += 1
+                else:
+                    res.append([nums[i], nums[l], nums[h]])
+                    while l < h and nums[l] == nums[l + 1]:
+                        l += 1
+                    while l < h and nums[h] == nums[h - 1]:
+                        h += 1
+                    l += 1
+                    h -= 1
 
-        self.printMatrix(matrix)
-            
-        for i in range(n):
-            for j in range(n // 2):
-                matrix[i][j], matrix[i][n-1-j] = matrix[i][n-1-j], matrix[i][j]
-
-        self.printMatrix(matrix)
-
-    def rotateBrute(self, matrix: List[List[int]]) -> None:
-        self.printMatrix(matrix)
-        mat = [[matrix[len(matrix) - 1 - j][i] for j in range(len(matrix[0]))] for i in range(len(matrix))]
-        matrix = mat
-        self.printMatrix(matrix)
-        return matrix
+        return res
 
 
 # for _ in range(3):
 inp = ast.literal_eval(input())
-res = Solution().rotate(inp)
+res = Solution().threeSum(inp)
 # inp1 = int(input())
 # inp2 = int(input())
 # res = Solution().pascalTriangleI(inp1, inp2)
