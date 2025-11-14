54c3cbf8bd1e617d40731ba16d54385167674e92
viswanathanTJ <viswanathantj@gmail.com>
2025-11-14 20:43:36 +0530

    maxSubArray

--- Python/solution.py
+++ Python/solution.py
@@ -6,27 +6,19 @@ import math
 
 class Solution:
 
-    def sortColors(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        n = len(nums)
-        ind = 0
-        l, r = 0, n - 1
-        while ind <= r:
-            if nums[ind] == 2:
-                nums[ind], nums[r] = nums[r], nums[ind]
-                r -= 1
-            elif nums[ind] == 0:
-                nums[ind], nums[l] = nums[l], nums[ind]
-                l += 1
-                ind += 1
-            else:
-                ind += 1
-        return nums
+    def maxSubArray(self, nums: List[int]) -> int:
+        res = -1
+        for i in range(len(nums)):
+            for j in range(i, len(nums) + 1):
+                csum = 0
+                for k in range(i, j):
+                    csum += nums[k]
+                    print(nums[k], end=',')
+                res = max(csum, res)
+                print(csum)
                 
-        
-    
+        return res
+
                 
             
                 
@@ -34,7 +26,7 @@ class Solution:
 # for _ in range(3):
 inp = ast.literal_eval(input())
 # inp1 = int(input())
-res = Solution().sortColors(inp)
+res = Solution().maxSubArray(inp)
 # inp2 = int(input())
 # res = Solution().pascalTriangleI(inp1, inp2)
 print(res)
